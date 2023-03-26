920121c39e4e979b625ae7112c5968c27dfa5155
DevEss <dev.shahryar@gmail.com>
2023-03-26 15:26:34 +0330

    added assets

---
+++ src/assets/index.js
@@ -0,0 +1,10 @@
+import easy from './easy.svg'
+import logo from './logo.svg'
+import menu from './menu.svg'
+import not_save from './not_save.svg'
+import star from './star.svg'
+import stars from './stars.svg'
+import strong from './strong.svg'
+import hero from './hero.svg'
+
+export { easy, logo, not_save, star, stars, strong, hero, menu }
