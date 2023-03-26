d4b5fbf24de10d5cad0f27afa596b02d42b19dac
DevEss <dev.shahryar@gmail.com>
2023-03-26 15:34:09 +0330

    hero added

---
+++ src/components/hero.jsx
@@ -0,0 +1,16 @@
+export default function Hero() {
+    return (
+        <div>
+            <div>
+                <h1>
+                    Remember Tria,
+                    <br />
+                    So you can forget your
+                    <br />
+                    old passwords!
+                </h1>
+            </div>
+            <div></div>
+        </div>
+    )
+}
