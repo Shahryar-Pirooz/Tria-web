dc038067eda5a819379e53d06c970124d50dcb2a
DevEss <dev.shahryar@gmail.com>
2023-03-27 11:53:17 +0330

    hero done

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,16 +1,33 @@
+import { hero } from '../assets'
 export default function Hero() {
     return (
-        <div>
-            <div>
-                <h1>
-                    Remember Tria,
+        <div className="hero">
+            <div className="hero-text-side">
+                <h1 className="hero-title">
+                    Remember <span className="text-blue">Tria</span>,
                     <br />
                     So you can forget your
                     <br />
                     old passwords!
                 </h1>
+                <h2 className="hero-description">
+                    Generate a super-secure password,
+                    <br />
+                    without having to remember it!
+                </h2>
+                <div className="buttons">
+                    <a className="btn-blue" href="#help">
+                        get start
+                    </a>
+                    <a className="btn-green" href="/">
+                        app
+                    </a>
+                </div>
             </div>
-            <div></div>
+
+            <div className="hero-image-side">
+                <img src={hero} alt="people thing about Tria!" />
+            </div>
         </div>
     )
 }
