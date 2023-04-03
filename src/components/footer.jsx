b9d6e5c1a2016aaadb93f9c21361a5528636ff92
DevEss <dev.shahryar@gmail.com>
2023-04-03 14:30:04 +0330

    footer done

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,3 +1,16 @@
 export default function Footer() {
-    return <>Footer</>
+    return (
+        <footer className="flex h-20 w-full items-center justify-center bg-blue text-center text-white">
+            <p className="flex text-sm">
+                © 2023 by{' '}
+                <a
+                    className="mx-2 font-extrabold"
+                    href="https://wizigners.com/"
+                >
+                    wizigners
+                </a>{' '}
+                team with magic and love.
+            </p>
+        </footer>
+    )
 }
